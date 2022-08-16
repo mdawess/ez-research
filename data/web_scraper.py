bd9bfa1e93c32fb4dd5099798406ae508930fd0c
mdawess <michaelanthony.dawes@mail.utornto.ca>
2022-08-16 12:15:20 -0600

    scraper returns full text along with a list

--- data/web_scraper.py
+++ data/web_scraper.py
@@ -44,15 +44,16 @@ def get_website_text(url: str, blacklist: List[str], unecessaary_words: List[str
     website = urllib.request.urlopen(url)
     soup = BeautifulSoup(website, "html.parser")
     website_text = soup.findAll(text = True)
+    output = ''
     list_output = []
 
     for t in website_text:
         if t.parent.name not in blacklist and \
         len(t) > 1 and not any(word in t.lower() for word in unecessaary_words):
-            # output += '{} '.format(t)
+            output += '{} '.format(t)
             list_output.append(t)
 
-    return list_output
+    return output, list_output
 
 if __name__ == '__main__':
     # Interesting lecture I read recently
