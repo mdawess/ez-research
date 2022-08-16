b1b023f6a0f6f9e94bbe824432861daf5501cfde
mdawess <michaelanthony.dawes@mail.utornto.ca>
2022-08-16 10:07:56 -0600

    added custom search function

--- data/query.py
+++ data/query.py
@@ -1,17 +1,52 @@
 
+from urllib import response
 import requests
 import os
 
 API_KEY = os.environ.get('GOOGLE_API_KEY')
-SEARCH_ENGINE_ID = os.environ.get('cw')
+CX = os.environ.get('CX')
+URL = "https://www.googleapis.com/customsearch/v1"
 
-TEST_QUERY = 'python'
+def custom_search(query: str, start: int) -> dict:
+    """
+    Custom search function using the custom search API.
+    """
+    inputs={
+        'key': API_KEY, 
+        'cx': CX, 
+        'q': query, 
+        'start': start
+    }
+    response = requests.get(URL, params=inputs).json()
+    search_time = response['searchInformation']['searchTime']
+    search_results = {}
 
-page = 1  # As a test
-start = (page - 1) * 10 + 1
+    for result in response['items']:
+        if 'author' in result['pagemap']['metatags'][0] and 'article:published_time' in result['pagemap']['metatags'][0]:
+            search_results[result['title']] = {
+                'url': result['formattedUrl'],
+                'author': result['pagemap']['metatags'][0]['author'],
+                'date': result['pagemap']['metatags'][0]['article:published_time'].split('T')[0],
+            }
+        elif 'author' in result['pagemap']['metatags'][0]:
+            search_results[result['title']] = {
+                'url': result['formattedUrl'],
+                'author': result['pagemap']['metatags'][0]['author'],
+                'date': 'Unknown',
+            }
+        elif 'article:published_time' in result['pagemap']['metatags'][0]:
+            search_results[result['title']] = {
+                'url': result['formattedUrl'],
+                'author': 'Unknown',
+                'date': result['pagemap']['metatags'][0]['article:published_time'].split('T')[0],
+            }
+        else:
+            search_results[result['title']] = {
+                'url': result['formattedUrl'],
+                'author': 'Unknown',
+                'date': 'Unknown',
+            }
+    return search_results, search_time
 
-url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={TEST_QUERY}&start={start}"
-
-data = requests.get(url).json()
-
-print(data)
+if __name__ == '__main__':
+    print(custom_search('Braised Short Ribs', 1))
