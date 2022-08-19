7b39860b57302e98f2cf128e0f2aac97961d19ed
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2022-08-18 19:13:20 -0600

    added theme change button

--- frontend/src/components/ThemeButton.tsx
+++ frontend/src/components/ThemeButton.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { useTheme } from 'next-themes'
+import { Sun, Moon } from 'react-bootstrap-icons';
 
 export default function ThemeButton() {
     const { theme, setTheme } = useTheme()
     return (
-        <div>
+        <div className='rounded-xl'>
             {theme === 'dark' ? 
                 <button className="text-white bg-gray-800"
-                onClick={() => setTheme('light')}
+                    onClick={() => setTheme('light')}
                 >
-                    
+                    <Moon size={28} />
                 </button>  
                 :
                 <button className="bg-white"
-                onClick={() => setTheme('dark')}
+                    onClick={() => setTheme('dark')}
                 >
-                    
+                    <Sun size={28} />
                 </button>
             }
         </div>
